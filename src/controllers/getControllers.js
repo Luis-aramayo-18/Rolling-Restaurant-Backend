a84053b774e2b46a8abb4498c1b7f1cea19b28fa
Jenny Gonzalez <jennygonzalez91@hotmail.com>
2023-02-17 22:38:36 -0300

    Configuracion inicial

--- src/controllers/getControllers.js
+++
@@ -1,9 +0,0 @@
-import ProductsDB from "../models/productSchema"
-
-export const getProducts = async (req,res)=>{
-    const data = await ProductsDB.find()
-
-    res.json({
-        data:data,
-    })
-}
--- src/helpers/randomID.js
+++
@@ -1,7 +0,0 @@
-export const randomID = () =>{
-    
-    let a = Date.now().toString(30)
-    let b = Math.random().toString(30).substring(2)
-
-    return a + b
-}
